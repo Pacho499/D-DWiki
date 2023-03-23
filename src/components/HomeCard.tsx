03bf3f5ec5e0755a0f2bd0417e4c2180f9957621
Lorenzo <lorenzopalumbo.dev@gmail.com>
2023-03-24 00:06:31 +0100

    layout fix

--- src/components/HomeCard.tsx
+++ src/components/HomeCard.tsx
@@ -16,11 +16,11 @@ const HomeCard: React.FC<HomeCardData> = ({title, description}) => {
   }, [language])
   
   return (
-    <div className='bg-main w-1/4'>
-      <div className='w-[70%] flex m-auto flex-col'>
+    <div className='bg-main w-1/4 relative mb-8'>
+      <div className='w-[70%] flex m-auto flex-col '>
         <h1 className='text-lg py-4 text-center'>{title}</h1>
-        <p className='text-base h-60'>{descriptionTranslate}</p>
-        <p className='p-2 my-4 bg-buttons rounded-lg text-center hover:bg-buttonsHover'>GO {title}</p>
+        <p className='text-base mb-20'>{descriptionTranslate}</p>
+        <p className='absolute bottom-0 left-10 p-2 my-4 bg-buttons rounded-lg text-center hover:bg-buttonsHover'>GO {title}</p>
       </div>
     </div>
   );
