126b96a15373177a577987be5b074f5cf24690b4
Lorenzo <lorenzopalumbo.dev@gmail.com>
2023-03-24 16:06:31 +0100

    add wiki page

---
+++ src/datas/WikiCardDatas.ts
@@ -0,0 +1,36 @@
+import { CardDatas } from "../@types/MyTypes";
+
+export const mainWikiCardDatas: Array<CardDatas> = [
+    {
+        title:'Game Rules',
+        description:'In this section you will find all the basic rules of the game like explanations on how to roll dice, calculate bonuses and penalties, and resolve conflicts'
+    },
+    {
+        title:'Spells',
+        description:'In this section you will find all the spells for the classes, you will find also a description of what is a spell and how to use it'
+    },
+    {
+        title:'Items',
+        description:'In this section you will find all the items that you can use in your adventure'
+    },
+    {
+        title:'Monsters',
+        description:'In this section you will find all the monsters that you can find in your adventure'
+    },
+    {
+        title:'PC Start',
+        description:'In this section you will find information on character creation the bases'
+    },
+    {
+        title:'PC Races',
+        description:'In this section you will find iformation based on all the races in the game'
+    },
+    {
+        title:'PC Classes',
+        description:'In this section you will find iformation based on all the classes in the game'
+    },
+    {
+        title:'PC Traits',
+        description:'In this section you will find iformation based on all the classes in the game'
+    },
+]
--- src/pages/Wiki.tsx
+++ src/pages/Wiki.tsx
@@ -1,9 +1,26 @@
+import { mainWikiCardDatas } from "../datas/WikiCardDatas"
+import { Link } from "react-router-dom"
 const Wiki = () => {
+
     return(
-        <div>
-            <h1>ciao sono wiki</h1>
+        <div className="pt-[10vh] min-h-[100vh]">
+            <h1 className="text-center text-xl text-white py-4 font-bold">Welcome to wiki section</h1>
+            <div className="grid-cols-4 w-3/4 m-auto grid gap-8 pb-10">
+               {mainWikiCardDatas.map(data => {
+                return(
+                    <div className="bg-main text-center rounded-md">
+                        <h1 className="font-bold text-lg my-6">{data.title}</h1>
+                        <p className="w-[80%] h-48 m-auto">{data.description}</p>
+                        <Link to={`/Wiki/${data.title}`}>
+                            <p className="p-1 bg-buttons hover:bg-buttonsHover w-1/2 m-auto rounded-lg mb-4">{`go ${data.title}`}</p>
+                        </Link>
+                        
+                    </div>
+                )
+            })} 
+            </div>
+            
         </div>
     )
 }
-
 export default Wiki
