d6283ca35cdc8b525ba722d5464f96e74ef77841
Lorenzo <lorenzopalumbo.dev@gmail.com>
2023-03-27 18:03:09 +0200

    add static datas

---
+++ src/datas/WikiDetaliDatas.ts
@@ -0,0 +1,95 @@
+import { wikiDetailDatas } from "../@types/MyTypes";
+
+export const WikiDatas: wikiDetailDatas = {
+    'Spells' : {
+        call:'classes',
+        textData:[
+            {
+                title:'What is a spell',
+                description: 'A spell is a magical ability that a character can cast to produce a variety of effects. Spells can be cast by characters who have the ability to use magic, such as wizards, sorcerers, clerics, and other spellcasting classes. A spell is typically composed of three components: the verbal component, the somatic component, and the material component. The verbal component is the spoken incantation that activates the spell, the somatic component is the physical gesture that accompanies the spell, and the material component is the physical object that is required to cast the spell.'
+            },
+            {
+                title:'How to use a spell',
+                description:'To use a spell in D&D, you must first have the necessary components. These components may include verbal incantations, somatic gestures, and material components such as a wand or a pouch of herbs. Once you have the necessary components, you must declare that you are casting the spell and describe the actions and words that your character is using to cast the spell. The Dungeon Master (DM) will then determine if the spell is successful, and what effects it has on the game world and any creatures or objects within it.'
+            },  
+        ]
+        
+    },
+    'Game Rules' : {
+        call:'',
+        textData:[]
+    },
+    'Items' : {
+        call:'equipment-categories',
+        textData:[
+            {
+                title:'Equipment',
+                description:'Opportunities abound to find treasure, equipment, weapons, armor, and more in the dungeons you explore. Normally, you can sell your treasures and trinkets when you return to a town or other settlement, provided that you can find buyers and merchants interested in your loot.'
+            }
+        ]
+    },
+    'Monsters' : {
+        call:'',
+        textData:[
+            {
+                title:'What are Monsters',
+                description:"In Dungeons & Dragons (D&D), monsters are non-player characters that players encounter during their adventures. Monsters can range from common animals and humanoids to fantastic creatures such as dragons, beholders, and demons. They are typically hostile to the players and may attack them on sight, though some can be reasoned with or even befriended. Each monster has its own set of abilities, strengths, and weaknesses, which the players must discover and exploit in order to defeat them. In D&D, monsters are a key part of the game's challenges and are essential to creating a sense of danger and excitement in the players' adventures."
+            }
+        ]
+    },
+    'PC Start'  : {
+        call:'',
+        textData:[
+            {
+                title:'Whats is a PC',
+                description:"a PC stands for 'player character', which is a character controlled by a player rather than the Dungeon Master (DM). PCs are the main protagonists of the game and are typically the focus of the story and action. Players create their own PCs by choosing a race, class, ability scores, and equipment, among other things. PCs are the players' avatars in the game world and have their own personalities, motivations, and backstories. Players control their PCs' actions during the game, including combat, exploration, and social interactions. As they progress through the game, PCs gain experience points and levels, which grant them new abilities, spells, and other benefits."
+            },
+            {
+                title: 'Ability Score',
+                description:"Ability scores are numerical values that represent a character's innate physical and mental abilities. The six ability scores are Strength, Dexterity, Constitution, Intelligence, Wisdom, and Charisma. Each ability score is determined by rolling a set of dice or using a point-buy system during character creation. These scores can then be modified by racial and class features, as well as by spells and magic items.",
+            },
+            {
+                title:'Alignment',
+                description:"Alignment is a broad categorization of a character's moral and ethical perspective. It describes a character's tendency towards good, evil, neutrality, law, or chaos, or a combination of these traits. The nine possible alignments in D&D are lawful good, neutral good, chaotic good, lawful neutral, true neutral, chaotic neutral, lawful evil, neutral evil, and chaotic evil."
+            },
+            {
+                title:'Background',
+                description:"Every story has a beginning. Your character's background reveals where you came from, how you became an adventurer, and your place in the world. Choosing a background provides you with important story cues about your character's identity."
+            },
+            {
+                title:'Language',
+                description:"Your race indicates the languages your character can speak by default, and your background might give you access to one or more additional languages of your choice."
+            },
+            {
+                title:'Skills',
+                description:"Each ability covers a broad range of capabilities, including skills that a character or a monster can be proficient in. A skill represents a specific aspect of an ability score, and an individual's proficiency in a skill demonstrates a focus on that aspect."
+            },
+        ]
+    },
+    'PC Races': {
+        call:'races',
+        textData:[
+            {
+                title : 'What are races',
+                description:"A race refers to a distinct species or group of humanoids that have common physical and cultural traits. Players choose a race for their character during character creation, which can affect their character's abilities, skills, and background. There are numerous races to choose from in D&D, including humans, elves, dwarves, halflings, gnomes, and various fantasy races such as orcs, goblins, and dragonborn. Each race has its own unique features, such as natural abilities, movement speeds, vision types, and skill proficiencies. Races in D&D are more than just physical traits; they also come with cultural traditions, beliefs, and customs."
+            },
+            {
+                title : 'What are subraces',
+                description:"Subraces are variations of a particular race that have slightly different physical or cultural traits, and sometimes offer additional abilities or traits. For example, the elf race has several subraces, including high elves, wood elves, and drow. High elves are known for their intelligence and magical abilities, while wood elves are quick and nimble with a connection to nature. Drow are dark elves with a reputation for evil and magic."
+            },
+        ]
+    },
+    'PC Classes' : {
+        call:'classes',
+        textData:[
+            {
+                title : 'What are classes',
+                description: "A character class is a fundamental part of the identity and nature of characters in the Dungeons & Dragons role-playing game. A character's capabilities, strengths, and weaknesses are largely defined by its class. A character's class affects a character's available skills and abilities."
+            },
+            {
+                title:'Class levels',
+                description:"A class level refers to the progression of a character's abilities within their chosen character class. A character's class determines their core abilities, such as combat skills, spellcasting abilities, and special abilities. When a character gains experience points (XP) through combat, exploration, and completing quests, they can level up and gain new abilities and features."
+            }
+        ]
+    },
+}
